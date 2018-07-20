bb03ea6ee161275025b9c10cdeae4151872b04c2
usupsuparma <usup.suparma7@gmail.com>
2018-07-20 16:58:44 +0700

    perbaikan modul

--- livecamera.py
+++ livecamera.py
@@ -4,8 +4,8 @@ vidio   = cv2.VideoCapture(0)
 
 while True:
     cond, frame = vidio.read()
-    edge = cv2.Canny(frame, 50,50)
-    cv2.imshow('Edge detect live video', edge)
+    # edge = cv2.Canny(frame, 50,50)
+    cv2.imshow('Edge detect live video', frame)
     exit = cv2.waitKey(1) & 0xff
 
     if exit == ord('x'):
